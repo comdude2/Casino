6edeffe977997a9632eb1a4fbd96122df302a682
Matt <comdude2@msn.com>
2015-05-04 23:18:15 +0100

    Variables added

--- src/net/mcviral/dev/plugins/casino/slots/SlotMachine.java
+++ src/net/mcviral/dev/plugins/casino/slots/SlotMachine.java
@@ -1,5 +1,19 @@
 package net.mcviral.dev.plugins.casino.slots;
 
 public class SlotMachine {
-
+	
+	private int id = -1;
+	private String tier = null;
+	private String world = null;
+	private int leverX = -1;
+	private int leverY = -1;
+	private int leverZ = -1;
+	private int frameX = -1;
+	private int frameY = -1;
+	private int frameZ = -1;
+	
+	public SlotMachine(){
+		
+	}
+	
 }
