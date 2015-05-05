f00bf4c9c48e577af8a2bb0cb158ba8bac18f865
nalo <ThePadna@users.noreply.github.com>
2015-05-05 03:14:32 +0100

    + Controller instance

--- src/net/mcviral/dev/plugins/casino/main/Casino.java
+++ src/net/mcviral/dev/plugins/casino/main/Casino.java
@@ -1,11 +1,14 @@
 package net.mcviral.dev.plugins.casino.main;
 
+import net.mcviral.dev.plugins.casino.slots.SlotMachineController;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Casino extends JavaPlugin{
+public class Casino extends JavaPlugin {
+
+	private SlotMachineController controller;
 	
 	public void onEnable(){
-		
+		controller = new SlotMachineController(this);
 	}
 	
 	public void onDisable(){
